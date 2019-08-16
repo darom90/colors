c4417ffc54ee751930ddfbbce6b63fca83d62194
darom.chhuon <darom.chhuon@PNC.LAN>
2019-08-16 08:16:06 +0700

    colors

---
+++ js/darom.js
@@ -0,0 +1,16 @@
+function sumNumber(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num1").value;
+    let sum = 0;
+    sum = Number(num1) + Number(num2);
+    alert(sum);
+}
+
+
+function minusNumber(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num1").value;
+    let minus = 0;
+    minus = Number(num1) - Number(num2);
+    alert(minus);
+}
